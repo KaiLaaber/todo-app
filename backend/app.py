b3de7bfb6a43e675fa7f6a14845787cead2de0c9
Kai Laaber <119350932+KaiLaaber@users.noreply.github.com>
2026-03-28 23:25:12 +0100

    Squashed commit of the following:
    commit 65e6ca803261858954074df62e3dd9b8b7d29e5b
    Author: Kai Laaber <119350932+KaiLaaber@users.noreply.github.com>
    Date:   Sat Mar 28 23:24:32 2026 +0100
    
        collected missing commits
    
    commit f122aebcd95fbcc8fc4906b4d13f73d284bf1b43
    Author: Kai Laaber <119350932+KaiLaaber@users.noreply.github.com>
    Date:   Sat Mar 28 23:20:01 2026 +0100
    
        added Stylesheet
    
    commit e7c388c39c6efdc83aa64778ae7fa6b1ec0cd426
    Author: Kai Laaber <119350932+KaiLaaber@users.noreply.github.com>
    Date:   Sat Mar 28 23:09:45 2026 +0100
    
        implemented toggle complete feature, implemented icons for buttons
    
    commit 64f88ea7c1d06d99d3773e64a8de771505832b39
    Author: Kai Laaber <119350932+KaiLaaber@users.noreply.github.com>
    Date:   Sat Mar 28 22:07:33 2026 +0100
    
        Database setup, Tasks are now persistent
    
    commit c14ebe408a1e4b7162d85cbc790e71904fe6dc83
    Author: Kai Laaber <119350932+KaiLaaber@users.noreply.github.com>
    Date:   Sat Mar 28 21:49:08 2026 +0100
    
        fixed title display when addTask
    
    commit b15c332a85a3a6a0463961bb98d5551da222c6ad
    Author: Kai Laaber <119350932+KaiLaaber@users.noreply.github.com>
    Date:   Sat Mar 28 21:32:25 2026 +0100
    
        added and partially fixed addTask method
    
    commit c635d6fca8fe596c272206a65651251c2e6768bb
    Author: Kai Laaber <119350932+KaiLaaber@users.noreply.github.com>
    Date:   Sat Mar 28 20:48:18 2026 +0100
    
        added html skeleton
    
    commit d19afed7934dcb11b13c39c96a5e4e0e7603b444
    Author: Kai Laaber <119350932+KaiLaaber@users.noreply.github.com>
    Date:   Sat Mar 28 20:44:33 2026 +0100
    
        added /tasks route
    
    commit 765ba5a31f62d9e30f428ba529f88d177c6c0128
    Author: Kai Laaber <119350932+KaiLaaber@users.noreply.github.com>
    Date:   Sat Mar 28 20:20:17 2026 +0100
    
        Frontend skeleton created
    
    commit 5115398b8cc0ddd1afd9761aac46087b33ec1c17
    Author: Kai Laaber <119350932+KaiLaaber@users.noreply.github.com>
    Date:   Sat Mar 28 20:18:10 2026 +0100
    
        API Structure created

---
+++ backend/app.py
@@ -0,0 +1,97 @@
+import sqlite3
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+def init_db():
+    conn = sqlite3.connect('tasks.db')
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER NOT NuLL,
+            title TEXT NOT NULL,
+            completed BOOLEAN NOT NULL DEFAULT 0
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+app = Flask(__name__)
+
+
+
+tasks = []
+task_id = 1
+
+
+@app.route('/')
+def home():
+    return 'Todo API is running!'
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    conn = sqlite3.connect('tasks.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM tasks')
+    rows = c.fetchall()
+
+    conn.close()
+    tasks = []
+
+    for row in rows:
+        tasks.append({
+            'id': row[0],
+            'title': row[1],
+            'completed': bool(row[2])
+        })
+
+    return jsonify(tasks)
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+
+    data = request.get_json()
+    
+    conn = sqlite3.connect('tasks.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO tasks (title) VALUES (?)', (data['title'],))
+
+    conn.commit()
+    conn.close()
+
+    return jsonify(data), 201
+
+@app.route('/tasks/<int:task_id>/toggle', methods=['PUT'])
+def toggle_task(task_id):
+    conn = sqlite3.connect('tasks.db')
+    c = conn.cursor()
+
+    c.execute('SELECT completed FROM tasks WHERE id = ?', (task_id,))
+    finishedTask = c.fetchone()
+
+    if finishedTask:
+        new_status = True if finishedTask[0] == False else False
+
+        c.execute('UPDATE tasks SET completed = ? WHERE id = ?', 
+                  (new_status, task_id))
+
+    conn.commit()
+    conn.close()
+    return jsonify({'message': 'Task updated'}), 200
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    conn = sqlite3.connect('tasks.db')
+    c = conn.cursor()
+
+    c.execute('DELETE FROM tasks WHERE id = ?', (task_id,))
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({'message': 'Task deleted'}), 200
+
+if __name__ == '__main__':
+    CORS(app)
+    init_db()
+    app.run(debug=True)
